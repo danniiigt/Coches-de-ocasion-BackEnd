29686c235415c7443f2d99e04a453291db0a20b5
Daniel Gonzalez <@danniiigt>
2022-08-31 23:22:31 +0200

    Removed useless log

--- routes/brand.js
+++ routes/brand.js
@@ -5,8 +5,6 @@ const Brand = require("../models/brand");
 router.get("/", async (req, res) => {
   const brands = await Brand.find().sort("name");
 
-  console.log(brands.name);
-
   res.json({
     brands,
   });
