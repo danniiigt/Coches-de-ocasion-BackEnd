ac5df79f25b555d946d4b22afe66bb1504ba54b2
Daniel Gonzalez <@danniiigt>
2022-08-31 23:21:22 +0200

    Sort A-Z car brands

--- routes/brand.js
+++ routes/brand.js
@@ -3,7 +3,9 @@ const router = Router();
 const Brand = require("../models/brand");
 
 router.get("/", async (req, res) => {
-  const brands = await Brand.find();
+  const brands = await Brand.find().sort("name");
+
+  console.log(brands.name);
 
   res.json({
     brands,
